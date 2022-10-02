387cd5af8437ad9d822e8ebe4f427891c2b21fb2
jason.chong <jason.chong@portcast.io>
2022-10-02 23:35:23 +0800

    updated to include all models

--- src/model.py
+++ src/model.py
@@ -2,10 +2,12 @@ from utils.fe_js import url_phishing_predictor_js
 from utils.fe_wd import url_phishing_predictor_wd
 from utils.fe_jl import url_phishing_predictor_jl
 from utils.fe_hx import url_phishing_predictor_hx
+from utils import fe_sk
 
 from keras.models import load_model
 import joblib
 import pickle
+import numpy as np
 
 dir = "models/"
 
@@ -29,13 +31,23 @@ NB_imported_wd = joblib.load(dir+"NB_WD")
 #HX
 XGB_imported_hx = joblib.load(dir+"XGBoostClassifierNew")
 
-print(url_phishing_predictor_js("https://wikipedia.com", pre_processor_imported_js, lg_imported_js, xgb_imported_js, rf_imported_js, nn_imported_js))
-print(url_phishing_predictor_wd("https://wikipedia.com", LR_imported_wd, SVC_imported_wd, NB_imported_wd))
-print(url_phishing_predictor_jl("https://wikipedia.com", DT_imported_jl, RF_imported_jl, ET_imported_jl))
-print(url_phishing_predictor_hx("https://wikipedia.com", XGB_imported_hx))
-
 #SK
 # lstm_model = keras.models.load_model("models/lstm")
 xgb_model = pickle.load(open(dir+"xgb.pkl", "rb"))
 gbc_model = pickle.load(open(dir+"gbc.pkl", "rb"))
 mlp_model = pickle.load(open(dir+"mlp.pkl", "rb"))
+
+def get_prediction(url):
+    jiesheng = url_phishing_predictor_js(url, pre_processor_imported_js, lg_imported_js, xgb_imported_js, rf_imported_js, nn_imported_js)
+    weedin = url_phishing_predictor_wd(url, LR_imported_wd, SVC_imported_wd, NB_imported_wd)
+    junlong = url_phishing_predictor_jl(url, DT_imported_jl, RF_imported_jl, ET_imported_jl)
+    huixian = url_phishing_predictor_hx(url, XGB_imported_hx)
+    extracted_url = fe_sk.feature_extraction(url)
+    extracted = np.array(extracted_url).reshape(-1,8)
+    szekee = gbc_model.predict(extracted)[0]
+    
+    #if 5 models we get 4 or more predicting phishing => output will be phishing (1)
+    if jiesheng+junlong+weedin+huixian+szekee >= 4:
+        return 1
+    else:
+        return 0
